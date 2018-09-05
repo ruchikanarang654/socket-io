5a34403fee4f54430868d4456cc4d6f1bb6b9280
sipl27 <ruchika.narang@systematixindia.com>
2018-09-05 19:42:17 +0530

    changed the port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var request = require('request');
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -45,8 +46,8 @@ io.on('connection', function (socket) {
 //     })
 //   });
 
-http.listen(3000, function () {
-  console.log('listening on *:3000');
+http.listen(port, function () {
+  console.log('listening on *:', port);
 });
 
 
