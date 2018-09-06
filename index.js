448daaab169871ea79da66105efa7e300b980082
sipl27 <ruchika.narang@systematixindia.com>
2018-09-06 12:37:51 +0530

    Change of token and added code to send message to specific port

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+  var socketId = socket.id;
   socket.on('chat message', function (msg) {
+    console.log('socket id:'+ socket.id);
     console.log('message: ' + msg);
     var inputbody = { "query_input": { "text": { "text": msg, "language_code": "en" } } }
 
     var headers = {
       //'Authorization': 'Bearer ' + inputs.token,
-      'Authorization': 'Bearer ya29.GlwPBooNNPBrY5P84ztDC0dnHLFdmg16_qDcU_RkaxS5KOtsJQXXL7CgCkT9B5TU7HPspG417sTDJBq-5UQq3qwpi6xXVH4-kSuQhfKvBuynNBd-WxzWX3AUAd4dWQ',
+      'Authorization': 'Bearer ya29.GlwQBv10p3yKQin_AQJ29opgFK6FFJ9CU2ZMPevES9BUDS71y9ORs9Yr-7RAzMMMh3lS2mYgb6fhGaLsrV9O1WJgHz2LDxRm1hcAXadmL-UALDWwoAelR1NqrrA2PA',
       'Content-Type': 'application/json; charset=utf-8'
     }
-    console.log('request-headers', headers);
+    
     var options = {
       // url: 'https://dialogflow.googleapis.com/v2/',
       url: 'https://dialogflow.googleapis.com/v2/projects/newagent-148ea/agent/sessions/123456789:detectIntent',
@@ -30,8 +32,8 @@ io.on('connection', function (socket) {
     request(options, function (error, response, body) {
 
       if (!error && response.statusCode === 200) {
-        console.log(body) // Print the json response
-        io.emit('chat message', body.queryResult.fulfillmentText);
+        io.to(`${socketId}`).emit('chat message', body.queryResult.fulfillmentText);
+        //io.emit('chat message', body.queryResult.fulfillmentText);
       }
     })
     io.emit('chat message', msg);
@@ -39,12 +41,6 @@ io.on('connection', function (socket) {
   });
 });
 
-//   io.on('connection', function(socket){
-//     console.log('a user connected');
-//     socket.on('disconnect', function(){
-//         console.log('user disconnected');
-//     })
-//   });
 
 http.listen(port, function () {
   console.log('listening on *:', port);
